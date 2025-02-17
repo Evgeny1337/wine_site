0fc586f5ee2babdbfb6be2ef07ab172d220b5e4f
Evgeny1337 <genya-sozykin@mail.ru>
2025-02-17 15:22:07 +0300

    add fill data from excel dict

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import datetime
 import pandas
+import collections
 import os
 
 
@@ -14,22 +15,19 @@ def years_with_correct_declension(years):
 
 
 def get_fill_data_from_excel():
-    excel_data = pandas.read_excel('wine.xlsx',
-                                   sheet_name='Лист1',
-                                   na_values=['', 'nan'],
-                                   keep_default_na=True)
-    data_list = excel_data.to_dict(orient='records')
-    wines = []
-    for row in data_list:
+    excel_data = pandas.read_excel('wine2.xlsx',
+                                   sheet_name='Лист1').fillna('')
+    excel_list = excel_data.to_dict(orient='records')
+    wine_list = collections.defaultdict(str)
+    for row in excel_list:
         wine = {
             'image': os.path.join('images', row.get('Картинка', '')),
             'name': row.get('Название', ''),
             'sort': row.get('Сорт', ''),
             'price': str(row.get('Цена', ''))
         }
-        wines.append(wine)
-
-    return wines
+        wine_list.setdefault(row['Категория'], []).append(wine)
+    return wine_list
 
 
 env = Environment(
@@ -37,13 +35,13 @@ env = Environment(
     autoescape=select_autoescape(['html', 'xml'])
 )
 
-wines = get_fill_data_from_excel()
+wine_list = get_fill_data_from_excel()
 
 year_count = datetime.datetime.now().year - 1920
 template = env.get_template('template.html')
 
 rendered_page = template.render(
-    wines=wines, year=years_with_correct_declension(year_count))
+    wine_list=wine_list, year=years_with_correct_declension(year_count))
 
 with open('index.html', 'w', encoding="utf8") as file:
     file.write(rendered_page)
